b0c72ccb2e8a069f6e2f8e9a7fca15d2897eada7
Mighty-k <tiolupopo@gmail.com>
2025-03-07 12:10:54 +0100

    cors

--- Backend/server.js
+++ Backend/server.js
@@ -8,6 +8,13 @@ const bodyparser = require('body-parser')
 const authRoutes = require('./routes/auth');
 const notesRoutes = require('./routes/notes');
 
+const corsOptions = {
+  origin: ['https://notes-app-27g5.vercel.app', 'https://notes-app-wheat-nu.vercel.app'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+};
+
+
 const app = express()
 // const PORT = process.env.PORT || 5000;
 
@@ -16,7 +23,7 @@ app.get('/', (req,res)=>{
 });
 
 // Middleware
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(bodyparser.json());
 
 // Routes
